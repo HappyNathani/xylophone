1a9e0c89f9c563371f03e39135dee007d69dd06d
HappyNathani <happynathani2003@gmail.com>
2023-01-09 15:57:58 +0530

    second commit

--- lib/xylophone_view.dart
+++ lib/xylophone_view.dart
@@ -20,32 +20,46 @@ class _XylophoneViewState extends State<XylophoneView> {
   ];
 
   List audios = [
-    "assets_note1.wav",
-    "assets_note2.wav",
-    "assets_note3.wav",
-    "assets_note4.wav",
-    "assets_note5.wav",
-    "assets_note6.wav",
-    "assets_note7.wav",
+    "note1.wav",
+    "note2.wav",
+    "note3.wav",
+    "note4.wav",
+    "note5.wav",
+    "note6.wav",
+    "note7.wav",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "XYLOPHONE",
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
       body: Column(
         children: List.generate(
           colours.length,
           (index) => Expanded(
-            child: ElevatedButton(
-              onPressed: () async {
-                AudioPlayer player = AudioPlayer();
-                await player.play(AssetSource(audios[index]));
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: colours[index],
-                shape: const RoundedRectangleBorder(),
+            child: GestureDetector(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: colours[index],
+                ),
               ),
-              child: Container(),
+              onTap: () {
+                setState(() {
+                  AudioPlayer player = AudioPlayer();
+                  player.play(
+                    AssetSource(
+                      audios[index],
+                    ),
+                  );
+                });
+              },
             ),
           ),
         ),
